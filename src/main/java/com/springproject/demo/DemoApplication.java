c483cfbd7a880d5be05e2a576f4d8bc2cac31e8c
saamirkc <samir.kc.a20@gmail.com>
2024-09-22 13:20:39 +0545

    commiting the @Bean and @Service annotations

--- src/main/java/com/springproject/demo/DemoApplication.java
+++ src/main/java/com/springproject/demo/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.springproject.demo;
 
 import com.springproject.demo.Service.MyService;
+import com.springproject.demo.Service.MyService2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication implements CommandLineRunner  {
 	@Autowired
 	private MyService myService;
+
+    @Autowired
+	private MyService2 myService2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -18,7 +23,16 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(myService.greet("samir !"));
+		String name=myService.greet("Samir !");
+		System.out.println(name);
 
+		String name2=myService2.sayHello("samir !");
+		System.out.println(name2);
 	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println(myService.greet("samir !"));
+//
+//	}
 }
