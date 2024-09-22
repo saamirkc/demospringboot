c668e8f61202e4f330d81cf0ceb0e01d2a2207b1
saamirkc <samir.kc.a20@gmail.com>
2024-09-22 13:12:14 +0545

    commot

--- src/main/java/com/springproject/demo/Config/MyConfig.java
+++ src/main/java/com/springproject/demo/Config/MyConfig.java
@@ -4,7 +4,7 @@ import com.springproject.demo.Service.MyService;
 import com.springproject.demo.Service.ServiceImpl.ServiceImplementation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.stereotype.Service;
 @Configuration
 public class MyConfig {
     @Bean
@@ -12,4 +12,7 @@ public class MyConfig {
         return new ServiceImplementation();
     }
 }
+
+//Since ServiceImplementation2 is annotated with @Service, it will be automatically detected and registered as a bean
+// so for MyService 2 no need to crate @Bean .
 
