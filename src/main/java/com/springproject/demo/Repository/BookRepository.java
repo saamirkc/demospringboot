f41427e9560362ecbc1dac0b21b2ae67644d6b0e
saamirkc <samir.kc.a20@gmail.com>
2024-09-24 19:33:05 +0545

    commit

--- src/main/java/com/springproject/demo/Repository/BookRepository.java
+++ src/main/java/com/springproject/demo/Repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.springproject.demo.Repository;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Book;
+
+public interface BookRepository extends JpaRepository<Book, String> {
+
 }
