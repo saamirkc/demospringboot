46eb8a6471d249c2ef0418e4d4e0cedb35382c5d
saamirkc <samir.kc.a20@gmail.com>
2024-09-24 21:09:57 +0545

    commit

---
+++ src/main/java/com/springproject/demo/ServiceMain/BookService.java
@@ -0,0 +1,16 @@
+package com.springproject.demo.ServiceMain;
+import com.springproject.demo.domain.Books;
+
+import java.util.List;
+
+public interface BookService {
+    public Books addBook(Books book);
+    public Books updateBook(Books book);
+
+
+  public  void deleteBook(String isbn);
+
+    public Books getBook(String isbn);
+    public List<Books> getAllBooks();
+
+}
