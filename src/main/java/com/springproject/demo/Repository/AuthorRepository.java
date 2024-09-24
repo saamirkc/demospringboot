a97b99dbfcbf1ebf2e5f2ffd64e0ac743d2a218d
saamirkc <samir.kc.a20@gmail.com>
2024-09-24 19:28:44 +0545

    commit

--- src/main/java/com/springproject/demo/Repository/AuthorRepository.java
+++ src/main/java/com/springproject/demo/Repository/AuthorRepository.java
@@ -1,4 +1,18 @@
 package com.springproject.demo.Repository;
 
-public class AuthorRepository {
+import com.springproject.demo.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+//    custom query can be defined here if needed
 }
+
+//Custom query methods in Spring Data repositories are useful when you need to perform specific queries
+// that are not covered by the default CRUD operations provided by interfaces like JpaRepository or CrudRepository.
+//example where custom query is needed :
+//finding user with a specific role and status
+//counting no. of orders for a particular customer
+//searching and filtering(Searching for books by title or author)
+//sorting and pagenation
+//in the above case normal CRUD operation cant perform above function so custom query is needed.
