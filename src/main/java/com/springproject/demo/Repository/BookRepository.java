ec258e4ced5fa6f0ffe78a00dc3ddc8a1ebe7a7a
saamirkc <samir.kc.a20@gmail.com>
2024-09-25 17:55:12 +0545

    Commit

--- src/main/java/com/springproject/demo/Repository/BookRepository.java
+++ src/main/java/com/springproject/demo/Repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.springproject.demo.Repository;
 
+import com.springproject.demo.domain.Author;
 import com.springproject.demo.domain.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface BookRepository extends JpaRepository<Books, String> {
-
+List<Books> findByAuthor(Author author);
 }
