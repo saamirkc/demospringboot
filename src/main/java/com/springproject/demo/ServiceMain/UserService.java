3b88d85b2fed65ec051ee5f63a98d09220befb52
saamirkc <samir.kc.a20@gmail.com>
2024-10-02 23:59:13 +0545

    commit

--- src/main/java/com/springproject/demo/ServiceMain/UserService.java
+++ src/main/java/com/springproject/demo/ServiceMain/UserService.java
@@ -2,12 +2,16 @@ package com.springproject.demo.ServiceMain;
 
 import com.springproject.demo.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     public User addUser(User user);
 
     public User getUser(String username);
 
     public void deleteUser(Long id);
+
+    public List<User> getUsers();
 
 
 }
