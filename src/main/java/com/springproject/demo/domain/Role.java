ab572154ab09e8fe824df2056bfd237c823b8061
saamirkc <samir.kc.a20@gmail.com>
2024-10-02 14:18:00 +0545

    commit

---
+++ src/main/java/com/springproject/demo/domain/Role.java
@@ -0,0 +1,27 @@
+package com.springproject.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonManagedReference
+    private List<User> users=new ArrayList<>();
+}
