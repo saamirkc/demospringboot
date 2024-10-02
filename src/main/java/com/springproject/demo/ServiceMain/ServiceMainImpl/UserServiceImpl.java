d95a4bc78be26ca8fff5d7efc6c21fa5f8430950
saamirkc <samir.kc.a20@gmail.com>
2024-10-02 14:22:49 +0545

    commit

---
+++ src/main/java/com/springproject/demo/ServiceMain/ServiceMainImpl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.springproject.demo.ServiceMain.ServiceMainImpl;
+
+import com.springproject.demo.Repository.UserRepository;
+import com.springproject.demo.ServiceMain.UserService;
+import com.springproject.demo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User addUser(User user) {
+    User user1= this.userRepository.findByUsername(user.getUsername());
+    if(user1==null){
+//        add new user:
+        user1=this.userRepository.save(user);
+    }
+    else{
+        throw new RuntimeException("User already exists");
+    }
+    return user1;
+
+    }
+    //here look at user service impl of exam portal backend .Since it uses intermediate userrole class for mapping between user and
+//    role so you have to explicitly manage the relationship between User and Role. This means when you create a user, you will
+//    also need to create or specify UserRole objects that link the user to their respective roles. so user role is also added ans saved.
+
+//    but here we are using direct mapping between user and role .so no need to save role while saving user as saving user can also
+//    save role because of JPA mapping between user and role.
+
+    @Override
+    public User getUser(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        this.userRepository.deleteById(id);
+    }
+}
