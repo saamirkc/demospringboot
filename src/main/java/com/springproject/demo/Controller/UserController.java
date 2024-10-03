c035c7ce699849a8753ab16c6ba1f0fb69a1f3f4
saamirkc <samir.kc.a20@gmail.com>
2024-10-03 22:50:04 +0545

    Commit

--- src/main/java/com/springproject/demo/Controller/UserController.java
+++ src/main/java/com/springproject/demo/Controller/UserController.java
@@ -1,6 +1,9 @@
 package com.springproject.demo.Controller;
 
+//import com.springproject.demo.ServiceMain.RoleService;
+import com.springproject.demo.ServiceMain.RoleService;
 import com.springproject.demo.ServiceMain.UserService;
+import com.springproject.demo.domain.Role;
 import com.springproject.demo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +17,8 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    @Autowired
+     private RoleService roleService;
     @PostMapping("/")
     public User addUser(@RequestBody User user) {
         return this.userService.addUser(user);
@@ -36,4 +40,26 @@ public class UserController {
         List<User>users=this.userService.getUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @GetMapping("/users/{roleName}")
+    public List<User>getRole(@PathVariable String roleName) {
+//        Role role=this.roleService.getRole(roleId);
+//        return this.userService.getRole(role);
+//        Role role=this.roleService.getRole(roleName);
+//        return this.userService.getUserByRole(role.getRoleName());
+
+        Role role=this.roleService.getRole(roleName);
+        return this.userService.findByRole(role);
+    }
+
+//    @GetMapping("/users/{roleName}")
+//    public ResponseEntity<List<User>> getRole(@PathVariable String roleName) {
+//        System.out.println("Role name requested: " + roleName);
+//        List<User> users = this.userService.getUserByRole(roleName);
+//        System.out.println("retrieved user :"+users);
+//        return new ResponseEntity<>(users, HttpStatus.OK);
+//
+//    }
+
+
 }
