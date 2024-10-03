585ee642e0b35ea957781826eba808dd791ab37d
saamirkc <samir.kc.a20@gmail.com>
2024-10-03 22:49:19 +0545

    Commit

--- src/main/java/com/springproject/demo/Repository/UserRepository.java
+++ src/main/java/com/springproject/demo/Repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.springproject.demo.Repository;
 
+import com.springproject.demo.domain.Role;
 import com.springproject.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByUsername(String username);
+
+    public List<User> findByRoles(Role role);
 }
