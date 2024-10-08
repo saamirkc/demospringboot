505de63c61a2396e6e90d801847e0576e2ec28da
saamirkc <samir.kc.a20@gmail.com>
2024-10-08 16:27:23 +0545

    commit

---
+++ src/main/java/com/springproject/demo/ConfigforJWT/JWTRequest.java
@@ -0,0 +1,14 @@
+package com.springproject.demo.ConfigforJWT;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTRequest {
+    private String username;
+    private String password;
+}
